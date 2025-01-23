cf22e4106814bf6d40e8855ace4dbeafa04d24a0
Matt Gregory <mattrhysgregory@gmail.com>
2025-01-23 20:18:05 +0000

    add support for line breaks

--- browser.py
+++ browser.py
@@ -152,9 +152,10 @@ def layout(text):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
+        if cursor_x >= WIDTH - HSTEP or c == "\n":
             cursor_y += VSTEP
             cursor_x = HSTEP
     return display_list
